9fa17ac1ee3a059256a6ba568c7f55e9e26edf33
Masa Lubbadeh <masa.lubbadeh@hotmail.com>
2024-11-24 14:49:57 +0200

    modifide the data model to store genres

--- lib/models/sign_up_data.dart
+++ lib/models/sign_up_data.dart
@@ -5,17 +5,20 @@ class SignUpData {
   String? phoneNumber;
   String? password;
   String? accountType; // New field for account type
+  List<String>? selectedGenres; // New field for selected genres
 
-  // Constructor to include accountType
+  // Constructor to include selectedGenres
   SignUpData({
     this.firstName,
     this.lastName,
     this.email,
     this.phoneNumber,
     this.password,
-    this.accountType, // Include accountType in constructor
+    this.accountType,
+    this.selectedGenres, // Include selectedGenres in constructor
   });
 
+  // Method to convert the object to JSON
   Map<String, dynamic> toJson() {
     return {
       "firstName": firstName,
@@ -24,6 +27,7 @@ class SignUpData {
       "phoneNumber": phoneNumber,
       "password": password,
       "accountType": accountType, // Add accountType to the map
+      "selectedGenres": selectedGenres, // Add selectedGenres to the map
     };
   }
 }
